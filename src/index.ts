0c2385d221ad5dcdad7e492701155e3b035b8fe9
Xaviju <xaviju@gmail.com>
2021-12-13 13:14:16 +0100

    🛂 Roxanne integration with languagetool

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import TelegramBot from "node-telegram-bot-api";
+import languagetool from "languagetool-api";
 import { token } from '../config/config';
+import { languagetoolParams } from "./model/languagetool.model";
+
+let params: languagetoolParams = {
+  language: 'en-GB',
+  text: '',
+  motherTongue: 'es',
+  level: 'default'
+}
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// ⚠️ Después de este comentario es donde ponemos la lógica de nuestro bot donde podemos crear los comandos y eventos para darle funcionalidades a nuestro bot
 
-// Implementación de la primera funcionalidad: Cuando mandamos el mensaje "Hola" reconoce tú nombre y genera un input tipo "Hola Daniel"
 
 bot.on('polling_error', function(error){
   console.log(error);
@@ -24,4 +31,24 @@ bot.onText(/^\/didiwin/, function(msg){
   var nameUser = msg.from.first_name;
 
   bot.sendMessage(chatId, "You have certainly won the PIWEEK, " + nameUser);
+});
+
+bot.onText(/^\/sentence/, function(msg){
+  var chatId = msg.chat.id;
+  params.text = "This is sentence"
+
+  languagetool.check(params, function(err, res){
+      if(err){
+         console.log(err);
+      } else{
+        console.log(res.matches[0]);
+        if (res.matches) {
+          bot.sendMessage(chatId, res.matches[0].shortMessage);
+          bot.sendMessage(chatId, res.matches[0].message);
+        } else {
+          bot.sendMessage(chatId, "This sentence looks good!");
+        }
+      }
+  });
+
 });
---
+++ src/model/languagetool.model.ts
@@ -0,0 +1,16 @@
+export interface languagetoolParams {
+  text: string;
+  data?: string;
+  language: string;
+  username?: string;
+  apiKey?: string;
+  dicts?: string;
+  motherTongue: string;
+  preferredVariants?: string;
+  enabledRules?: string;
+  disabledRules?:string;
+  enabledCategories?: string;
+  disabledCategories?: string;
+  enabledOnly?: string;
+  level: string;
+}
